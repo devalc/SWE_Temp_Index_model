823ea21958051b435542db3ea95bc9886f675593
devalc <deva4998@vandals.uidaho.edu>
2018-11-05 13:38:31 -0800

    test

--- SWE.py
+++ SWE.py
@@ -10,83 +10,32 @@ Created on Tue Sep 04 18:52:27 2018
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from functions import *
+from scipy import optimize as op
 
 #p0 = [tbase, train, tsnow,k]
 p0 = [0,3,0,2.29]
 filepath= 'D:/Chinmay/Hydro_modeling_erin_fall_2018/snotel_623_pre_mgmt.csv'
-SNOF = 1.55
-Tscrit = 2.9
-
-def process_data(filepath):
-    data = pd.read_csv(filepath, nrows = 6954,parse_dates=['Date'], skiprows=1,\
-                       names= ['Date', 'SWE_obs','P_accum','T_max','T_min',\
-                               'T_avg','P_incremental']).dropna()
-    P = data['P_accum'].values
-    T_avg = data['T_avg'].values
-    SWE_obs= data['SWE_obs'].values
-    return data, P, T_avg, SWE_obs
-
-    
-
-def SWE_usace(p0, T_avg):
-    dt = T_avg.size
-    Mf  = np.zeros(dt)
-    Ps  = np.zeros(dt)
-    SWE = np.zeros(dt)
-    for t in np.arange(1,dt,1):
-        if (SWE_obs[t-1] > 0.0) & (T_avg[t] > p0[0]):
-            Mf[t] = np.minimum((p0[3]*(T_avg[t] - p0[0])), SWE_obs[t-1])
-        if (P[t]>0.0):
-            if (T_avg[t]-Tscrit):
-                Ps[t] = SNOF*P[t]
-            else:
-                Ps[t] = 0.0
-    
-        SWE[t] = SWE[t-1] + Ps[t] - Mf[t]
-        
-    return SWE
-            
-
-def plot_res():
-     fig= plt.figure(figsize=(14,24))
-     plt.subplot(411)
-     plt.plot(data['Date'],P,'r-')
-     plt.ylabel('Precipitation [mm]')
-     plt.subplot(412)
-     plt.plot(data['Date'],T_avg,'r-')
-     plt.ylabel('Temperature [${}^\circ$C]')
-     plt.subplot(413)
-     plt.plot(data['Date'],Mf,'b-')
-     plt.ylabel('Melt flux [mm/day]')
-     plt.subplot(414)
-     plt.plot(data['Date'],SWE,'b-')
-     plt.plot(data['Date'],SWE_obs,'ko')
-     plt.xlabel('Date')
-     plt.ylabel('SWE [mm]')
-     plt.legend(('Modeled SWE','Observed SWE'))
-     return fig
-    
-    
-    
-    
-    data = datain
-    data['SWE.melt'] = np.where((data['Air Temperature Average (degC)']-p0[0])*p0[3] <=0, \
-        0,(data['Air Temperature Average (degC)']-p0[0])*p0[3])
-    
-    data['SWE.sim'] = np.where((data['Air Temperature Average (degC)']<p0[1] & data['Air Temperature Average (degC)'] > p0[2]),
-                        data['P']*((data['Air Temperature Average (degC)']-p0[2])/(p0[1]-p0[2])),0)
-    return data
 
 
 data, P, T_avg, SWE_obs = process_data(filepath)
 
-melt = SWE_usace(p0,data)
+melt,Psnow,Prain = Snowmelt_DD_usace(p0, P,T_avg, Tsnow=p0[2], Train=p0[1],k=p0[3])
 
-#    
-#    if (T_avg - p0[0])*p0[3] <= 0:
-#        swe_melt = 0
-#    else:
-#        swe_melt = (T_avg - p0[0])*p0[3]
-#        return swe_melt
-#        
+cumSWE, act_melt = simSWE(Psnow, melt)
+
+
+data['cumSWE']= cumSWE[:]
+data['melt']= melt[:]
+data['Psnow']= Psnow[:]
+data['Prain']= Prain[:]
+data['act_melt']= act_melt[:]
+
+md = md(SWE_obs, cumSWE)
+rmse= rmse(SWE_obs, cumSWE)
+nse= nse(SWE_obs, cumSWE)
+
+
+plot_res(data)
+
+op.minimize(rmse, p0[3])
---
+++ functions.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 10 12:51:29 2018
+
+@author: Chinmay Deval
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def process_data(filepath):
+    data = pd.read_csv(filepath, nrows = 6954,parse_dates=['Date'], skiprows=1,\
+                       names= ['Date', 'SWE_obs','P_accum','T_max','T_min',\
+                               'T_avg','P_incremental']).dropna()
+    P = data['P_incremental'].values
+    T_avg = data['T_avg'].values
+    SWE_obs= data['SWE_obs'].values
+    return data, P, T_avg, SWE_obs
+
+def Snowmelt_DD_usace(p0, P, T,  Tsnow,Train, k, Tbase=0.0):
+    #melt
+    melt= k*(T-Tbase)
+    melt[melt<0.0]=0.0
+    # Psnow and Prain
+    Psnow= np.where(T < Train, np.where(T>Tsnow, np.multiply(P, np.divide(np.subtract(T,Tsnow),\
+                                                                (Train-Tsnow))), P), 0.0)
+    Prain = np.subtract(P, Psnow)
+    
+    return melt, Psnow, Prain
+
+
+def simSWE(Psnow, meltflux):
+    cumSWE = np.zeros(Psnow.shape, dtype=np.float32)
+    act_melt = np.zeros(meltflux.shape, dtype=np.float32)
+    for t in range(1,Psnow.shape[0]):
+        swe_inc = cumSWE[t-1] + Psnow[t] - meltflux[t]
+        
+        if swe_inc > 0.0:
+            cumSWE[t] = swe_inc
+            act_melt[t] = meltflux[t]
+        else:
+            act_melt[t] = cumSWE[t-1]
+
+    return cumSWE, act_melt
+
+
+def md(sim, obs):
+    """
+    Mean difference 
+
+    """
+    return(np.mean(np.subtract(sim, obs)))
+
+def nse(sim, obs):
+    """
+    Nash-Sutcliffe Efficiency
+
+    """
+    obs_mod2 = np.sum(np.square(np.subtract(obs, sim)))
+    obs_mean2 = np.sum(np.square(np.subtract(obs, np.mean(obs))))
+    nse = 1-(obs_mod2/obs_mean2)
+    return nse
+
+def rmse(sim, obs):
+    """
+    
+    """
+    return(np.nanmean(np.sqrt(np.square(np.subtract(sim, obs)))))
+
+
+def plot_res(data):
+    """
+    takes in dataframe with date and calculated variables
+    
+    returns plots of P, T, Meltflux, and Observed and Predicted SWE
+    """
+    fig= plt.figure(figsize=(14,24))
+    plt.subplot(411)
+    plt.plot(data.iloc[:,0],data.iloc[:,6],'r-')
+    plt.ylabel('Precipitation [mm]')
+    plt.subplot(412)
+    plt.plot(data.iloc[:,0], data.iloc[:,5],'r-')
+    plt.ylabel('Temperature [${}^\circ$C]')
+    plt.subplot(413)
+    plt.plot(data.iloc[:,0],data.iloc[:,11],'b-')
+    plt.ylabel('Melt flux [mm/day]')
+    plt.subplot(414)
+    plt.plot(data.iloc[:,0],data.iloc[:,7],'b-')
+    plt.plot(data.iloc[:,0],data.iloc[:,1],'ko')
+    plt.xlabel('Date')
+    plt.ylabel('SWE [mm]')
+    plt.legend(('Modeled SWE','Observed SWE'))
+    
+    return fig
+
+
